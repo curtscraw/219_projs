2d58e48b3c304f96d37a112b78cdaf6e4ac604af
Curtis Crawford <curtiscraw@gmail.com>
2018-02-05 20:57:35 -0800

    part3 done, part5 code ready for runs

--- lab2/20_cats.py
+++ lab2/20_cats.py
@@ -11,13 +11,17 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction import text
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import sys
 
+if len(sys.argv) < 2:
+    print "This script must be called with an r-value for reduction, for instance:"
+    print "python 20_cats.py <r value>"
+
+rval = int(sys.argv[1])
 #setup, fetch datasets
 this_df = 3
 
-categories = None
-
-dataset = fetch_20newsgroups(subset='all', categories=categories, shuffle=True, random_state=42);
+dataset = fetch_20newsgroups(subset='all', shuffle=True, random_state=42);
 
 #vectorize dataset, find tfidf, report dimensions
 vectorizer = text.TfidfVectorizer(min_df=this_df, stop_words='english')
@@ -25,9 +29,34 @@ tfidf = vectorizer.fit_transform(dataset.data)
 print "TFxIDF dimensions: " + str(tfidf.shape)
 
 #reduce dimensionality
+from sklearn.decomposition import NMF
+from sklearn.decomposition import TruncatedSVD
+lsi = TruncatedSVD(n_components=rval, random_state=0)
+nmf = NMF(n_components=rval, init='random', random_state=0)
 
 #cluster
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import homogeneity_score,completeness_score,v_measure_score
 from sklearn.metrics.cluster import adjusted_rand_score,adjusted_mutual_info_score
-km = KMeans(n_clusters=20, init='k-means++', max_iter=100, n_init=1)
+from sklearn.metrics import confusion_matrix
+
+
+def print_scores(labels, predicted):
+    print "Homogeneity: " + str(homogeneity_score(labels, predicted))
+    print "completeness: " + str(completeness_score(labels, predicted))
+    print "V-measure: " + str(v_measure_score(labels, predicted))
+    print "RAND score: " + str(adjusted_rand_score(labels, predicted))
+    print "Mutual Info: " + str(adjusted_mutual_info_score(labels, predicted))
+    
+#returns [homogeneity, completeness, v_measure, rand, mutualr_info] for plotting 
+def km_score(labels, data):
+    km = KMeans(n_clusters=2, init='k-means++', max_iter=100, n_init=1)
+    km.fit(data)
+    print_scores(labels, km.labels_)
+
+print "With r=" + str(rval) + ":"
+print "SVD:"
+km_score(dataset.target, lsi.fit_transform(tfidf))
+print "NMF:"
+km_score(dataset.target, nmf.fit_transform(tfidf))
+
--- lab2/8_cats.py
+++ lab2/8_cats.py
@@ -29,6 +29,7 @@ def class_combine(in_targets):
             out.append(1)
 
     return (out, ('comp-tech', 'rec'))
+
 print "Relabeling data with either 0 for comp-tech or 1 for rec"
 labels, label_names = class_combine(dataset.target)
 
@@ -42,21 +43,115 @@ print "TFxIDF dimensions: " + str(tfidf.shape)
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import homogeneity_score,completeness_score,v_measure_score
 from sklearn.metrics.cluster import adjusted_rand_score,adjusted_mutual_info_score
-km = KMeans(n_clusters=2, init='k-means++', max_iter=100, n_init=1)
+from sklearn.metrics import confusion_matrix
 
 print "Clustering sparse tfidf with kmeans, k=2"
-km.fit(tfidf)
 
-print "Homogeneity: " + str(homogeneity_score(labels, km.labels_))
-print "completeness: " + str(completeness_score(labels, km.labels_))
-print "V-measure: " + str(v_measure_score(labels, km.labels_))
-print "RAND score: " + str(adjusted_rand_score(labels, km.labels_))
-print "Mutual Info: " + str(adjusted_mutual_info_score(labels, km.labels_))
+#returns [homogeneity, completeness, v_measure, rand, mutualr_info] for plotting 
+def print_scores(labels, predicted):
+    print "Contingency: "
+    print str(confusion_matrix(labels, predicted))
+
+    ret = [] 
+    ret.append(homogeneity_score(labels, predicted))
+    ret.append(completeness_score(labels, predicted))
+    ret.append(v_measure_score(labels, predicted))
+    ret.append(adjusted_rand_score(labels, predicted))
+    ret.append(adjusted_mutual_info_score(labels, predicted))
+
+    print "Homogeneity: " + str(homogeneity_score(labels, predicted))
+    print "completeness: " + str(completeness_score(labels, predicted))
+    print "V-measure: " + str(v_measure_score(labels, predicted))
+    print "RAND score: " + str(adjusted_rand_score(labels, predicted))
+    print "Mutual Info: " + str(adjusted_mutual_info_score(labels, predicted))
+    
+    return ret
+
+#returns [homogeneity, completeness, v_measure, rand, mutualr_info] for plotting 
+def km_score(labels, data):
+    km = KMeans(n_clusters=2, init='k-means++', max_iter=100, n_init=1)
+    km.fit(data)
+
+    return print_scores(labels, km.labels_)
+
+#don't care to save the returned values
+temp = km_score(labels, tfidf)
 
 #part 3
-from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import NMF
-#TODO
+from sklearn.decomposition import TruncatedSVD
 
+#run truncated svd with target of 1000, get variance retained by each of fist 1000 principal components
+lsi = TruncatedSVD(n_components=1000, random_state=0)
+lsi.fit(tfidf)
+lsi_var = lsi.explained_variance_ratio_.cumsum().tolist()
+
+#3ai, only need to do SVD! thank goodness!
+rvals = range(1, 1001)
+plt.plot(rvals, lsi_var, label="SVD")
+plt.xlabel('r value')
+plt.ylabel('Percent of variance retained')
+plt.title('Variance retained vs r value for Truncated SVD')
+plt.show()
+
+#truncated svd version
+nmf_scores = []
+lsi_scores = []
+key_r = [1, 2, 3, 5, 10, 20, 50, 100, 300]
+for i in key_r:
+    nmf = NMF(n_components=i, init='random', random_state=0)
+    lsi = TruncatedSVD(n_components=i, random_state=0)
+
+    print "NMF with r=" + str(i)
+    nmf_scores.append(km_score(labels, nmf.fit_transform(tfidf)))
+    print "SVD with r=" + str(i)
+    lsi_scores.append(km_score(labels, lsi.fit_transform(tfidf)))
+
+#plots for part 3aii
+#homogeneity
+plt.plot(key_r, [pt[0] for pt in lsi_scores], label="SVD")
+plt.plot(key_r, [pt[0] for pt in nmf_scores], label="NMF")
+plt.xlabel('r value')
+plt.ylabel('Homogeneity Score')
+plt.title('Homogeneity Score relative to r value')
+plt.legend(loc="lower right")
+plt.show()
+
+#completeness
+plt.plot(key_r, [pt[1] for pt in lsi_scores], label="SVD")
+plt.plot(key_r, [pt[1] for pt in nmf_scores], label="NMF")
+plt.xlabel('r value')
+plt.ylabel('Completeness Score')
+plt.title('Completeness Score relative to r value')
+plt.legend(loc="lower right")
+plt.show()
+
+#v_measure
+plt.plot(key_r, [pt[2] for pt in lsi_scores], label="SVD")
+plt.plot(key_r, [pt[2] for pt in nmf_scores], label="NMF")
+plt.xlabel('r value')
+plt.ylabel('V-measure Score')
+plt.title('V-measure Score relative to r value')
+plt.legend(loc="lower right")
+plt.show()
+
+#RAND-index
+plt.plot(key_r, [pt[3] for pt in lsi_scores], label="SVD")
+plt.plot(key_r, [pt[3] for pt in nmf_scores], label="NMF")
+plt.xlabel('r value')
+plt.ylabel('RAND Score')
+plt.title('Adjusted RAND relative to r value')
+plt.legend(loc="lower right")
+plt.show()
+
+#mutual info
+plt.plot(key_r, [pt[4] for pt in lsi_scores], label="SVD")
+plt.plot(key_r, [pt[4] for pt in nmf_scores], label="NMF")
+plt.xlabel('r value')
+plt.ylabel('Mutual Info Score')
+plt.title('Adjusted Mutual Info Score relative to r value')
+plt.legend(loc="lower right")
+plt.show()
+ 
 #part 4
 #TODO
